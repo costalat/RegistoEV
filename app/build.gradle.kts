5405f1a513832deb59e60007d7162ac86e11530d
costalat <costa_lat@yahoo.com>
2026-06-08 14:03:13 +0100

    Upgrade to V1.7.7: Enhanced Geographic Search with JSON Asset and Town Fallback

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         minSdk = 26
         targetSdk = 36
         versionCode = 7
-        versionName = "1.7"
+        versionName = "1.7.7"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
